06d2c952baf12ad146af451f4051982aa00e6937
yothipriya2002 <pandugajyothi259@gmail.com>
2024-12-05 13:03:52 +0530

    update

---
+++ Server Side/utils/db.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql2';
+
+// Create the MySQL connection with the correct credentials
+const Connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '2002', // Make sure this matches your MySQL root password
+  database: 'emp', // Replace with your database name
+});
+
+Connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL: ', err); 
+    return;
+  }
+  console.log('Connected to MySQL');
+});
+ 
+export default Connection
+
+
