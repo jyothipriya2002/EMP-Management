e2fedeae5d869eb9a72bfd2ad60e9e6acaaada82
yothipriya2002 <pandugajyothi259@gmail.com>
2024-12-11 08:13:31 +0530

    Fix start script for deployment

--- Server Side/Routes/AdminRoute.js
+++ Server Side/Routes/AdminRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
 // import con from "../utils/db.js";
 import jwt from "jsonwebtoken";
-import bcrypt from 'bcrypt'
+ import bcrypt from 'bcrypt';
+
 import multer from "multer";
 import path from "path";
 import Connection from "../utils/db.js";
